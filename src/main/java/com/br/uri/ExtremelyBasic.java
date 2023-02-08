d34900a5c86146bfbdffb822f5615e1e8ee41fa4
Mayndi <mayndi15@gmail.com>
2023-02-08 09:55:23 -0300

    initial commit

---
+++ src/main/java/com/br/uri/ExtremelyBasic.java
@@ -0,0 +1,23 @@
+package com.br.uri;
+
+import java.util.Scanner;
+
+public class ExtremelyBasic {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A;
+        int B;
+        int X;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        X = A + B;
+
+        System.out.println("X = " + X);
+    }
+}
+
+
